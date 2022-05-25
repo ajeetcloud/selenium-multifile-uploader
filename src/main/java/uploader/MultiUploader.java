c7187db419557088423a0f09c5f1382c09728889
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2022-05-25 09:58:27 +0530

    Initial commit
    Initial commit

---
+++ src/main/java/uploader/MultiUploader.java
@@ -0,0 +1,34 @@
+package uploader;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiUploader {
+    public static void main(String[] args) {
+        WebDriver driver = getWebDriver();
+        performUploads(driver);
+        driver.close();
+    }
+
+    public static WebDriver getWebDriver() {
+        System.setProperty("webdriver.chrome.driver", "G:\\Downloads\\General\\tools\\[teddyfleece]\\New folder\\New folder\\Process\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        return driver;
+    }
+
+    public static void performUploads(WebDriver driver) {
+        // <input type="file">
+        WebElement multiFileInput = driver.findElement(By.id("fileToUpload"));
+        List<String> filePaths = new ArrayList<>();
+        for (String path : filePaths) {
+            multiFileInput.sendKeys(path);
+        }
+        // Here you can go ahead and perform rest of the actions like submitting the form.
+    }
+}
